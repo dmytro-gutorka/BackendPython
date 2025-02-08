93bd5210d2d1ca3c1f1c456041104c7e65d04dbc
dimagutorka <dgutorka@gmail.com>
2025-02-08 14:04:16 +0200

    Fibonachi numbers with cache

--- hw27-2/fibo.py
+++ hw27-2/fibo.py
@@ -1,17 +1,29 @@
-from functools import lru_cache
+import redis
 
 
-@lru_cache(maxsize=None)
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+
+
 def fibonacci(n):
     if n < 0:
-        raise ValueError("Nubmer cannot be negative")
-    elif n == 0:
+        raise ValueError("Number cannot be negative")
+
+    cached_value = redis_client.get(f"fibonacci:{n}")
+    if cached_value:
+        return int(cached_value)
+
+    if n == 0:
         return 0
     elif n == 1:
         return 1
-    return fibonacci(n - 1) + fibonacci(n - 2)
+
+    fib_value = fibonacci(n - 1) + fibonacci(n - 2)
+
+    redis_client.set(f"fibonacci:{n}", fib_value)
+
+    return fib_value
 
 
 if __name__ == "__main__":
-    n = 10
-    print(f"Fibonacci({n}) =", fibonacci(n))
+    n = 100
+    print(f"Fibonacci({n}) = {fibonacci(n)}")
